9362a7c55ad6d9feb9d054d50b20464a42a6cb9d
YaoLi3 <liyaoo6o@gmail.com>
2023-03-15 16:48:39 +0800

    add plotting module

---
+++ spatialGRN/plot.py
@@ -0,0 +1,344 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@file: regulatory_network.py
+@time: 2023/Jan/08
+@description: inference gene regulatory networks
+@author: Yao LI
+@email: liyao1@genomics.cn
+@last modified by: Yao LI
+
+change log:
+    2023/01/08 init
+"""
+
+# python core modules
+
+# third party modules
+import anndata
+import logging
+import pandas as pd
+import numpy as np
+import scanpy as sc
+from pyscenic.cli.utils import load_signatures
+from pyscenic.rss import regulon_specificity_scores
+from stereo.core.stereo_exp_data import StereoExpData
+
+# modules in self project
+
+logger = logging.getLogger()
+
+
+class PlotRegulatoryNetwork:
+    """
+    Plot Gene Regulatory Networks related plots
+    """
+
+    def __init__(self, data):
+        super(PlotRegulatoryNetwork, self).__init__(data)
+        self._regulon_list = None
+        self._auc_mtx = None
+        self._regulon_dict = None
+
+    @property
+    def regulon_list(self):
+        return self._regulon_list
+
+    @regulon_list.setter
+    def regulon_list(self, value):
+        self._regulon_list = value
+
+    @property
+    def regulon_dict(self):
+        return self._regulon_dict
+
+    @regulon_dict.setter
+    def regulon_dict(self, value):
+        self._regulon_dict = value
+
+    @property
+    def auc_mtx(self):
+        return self._auc_mtx
+
+    @auc_mtx.setter
+    def auc_mtx(self, value):
+        self._auc_mtx = value
+
+    # dotplot method for anndata
+    @staticmethod
+    def dotplot_anndata(data: anndata.AnnData,
+                        gene_names: list,
+                        cluster_label: str,
+                        save: bool = True,
+                        **kwargs):
+        """
+        create a dotplot for Anndata object.
+        a dotplot contains percent (of cells that) expressed (the genes) and average expression (of genes).
+
+        :param data: gene data
+        :param gene_names: interested gene names
+        :param cluster_label: label of clustering output
+        :param save: if save plot into a file
+        :param kwargs: features Input vector of features, or named list of feature vectors
+        if feature-grouped panels are desired
+        :return: plt axe object
+        """
+        if isinstance(data, anndata.AnnData):
+            return sc.pl.dotplot(data, var_names=gene_names, groupby=cluster_label, save=save, **kwargs)
+        elif isinstance(data, StereoExpData):
+            logger.info('for StereoExpData object, please use function: dotplot_stereo')
+
+    # dotplot method for StereoExpData
+    @staticmethod
+    def _cal_percent_df(exp_matrix: pd.DataFrame,
+                        cluster_meta: pd.DataFrame,
+                        regulon_genes: str,
+                        celltype: list,
+                        groupby: str,
+                        cutoff: float = 0):
+        """
+        Expression percent
+        cell numbers
+        :param exp_matrix:
+        :param cluster_meta:
+        :param regulon_genes:
+        :param celltype:
+        :param cutoff:
+        :return:
+        """
+        # which cells are in cluster X
+        cells = cluster_meta[cluster_meta[groupby] == celltype]['cell']
+        ncells = set(exp_matrix.index).intersection(set(cells))
+        # get expression data for cells
+        ct_exp = exp_matrix.loc[ncells]
+        # input genes in regulon Y
+        # get expression data for regulon Y genes in cluster X cells
+        g_ct_exp = ct_exp[regulon_genes]
+        # count the number of genes which expressed in cluster X cells
+        regulon_cell_num = g_ct_exp[g_ct_exp > cutoff].count().count()
+        total_cell_num = g_ct_exp.shape[0] * g_ct_exp.shape[1]
+        if total_cell_num == 0:
+            return 0
+        else:
+            return regulon_cell_num / total_cell_num
+
+    @staticmethod
+    def _cal_exp_df(exp_matrix, cluster_meta, regulon_genes, celltype: str, groupby: str):
+        """
+        Calculate average expression level for regulon Y genes in cluster X cells
+        :param exp_matrix:
+        :param cluster_meta:
+        :param regulon_genes:
+        :param celltype
+        :return: numpy.float32
+        """
+        # get expression data for regulon Y genes in cluster X cells
+        cells = cluster_meta[cluster_meta[groupby] == celltype]['cell']
+        ncells = set(exp_matrix.index).intersection(set(cells))
+        ct_exp = exp_matrix.loc[ncells]
+        g_ct_exp = ct_exp[regulon_genes]
+        if g_ct_exp.empty:
+            return 0
+        else:
+            return np.mean(g_ct_exp)
+
+    @staticmethod
+    def dotplot_stereo(data: StereoExpData,
+                       meta: pd.DataFrame,
+                       regulon_dict,
+                       regulon_names: list,
+                       celltypes: list,
+                       groupby: str,
+                       palette: str = 'RdYlBu_r',
+                       **kwargs):
+        """
+        Intuitive way of visualizing how feature expression changes across different
+        identity classes (clusters). The size of the dot encodes the percentage of
+        cells within a class, while the color encodes the AverageExpression level
+        across all cells within a class (blue is high).
+
+        :param data:
+        :param meta:
+        :param regulon_dict:
+        :param regulon_names:
+        :param celltypes:
+        :param groupby:
+        :param palette:
+        :param kwargs: features Input vector of features, or named list of feature vectors
+        if feature-grouped panels are desired
+        :return:
+        """
+        expr_matrix = data.to_df()
+        dot_data = {'cell type': [], 'regulons': [], 'percentage': [], 'avg exp': []}
+
+        for reg in regulon_names:
+            target_genes = regulon_dict[f'{reg}(+)']
+            for ct in celltypes:
+                reg_ct_percent = PlotRegulatoryNetwork._cal_percent_df(exp_matrix=expr_matrix,
+                                                                       cluster_meta=meta,
+                                                                       regulon_genes=target_genes,
+                                                                       celltype=ct, groupby=groupby)
+                reg_ct_avg_exp = PlotRegulatoryNetwork._cal_exp_df(exp_matrix=expr_matrix,
+                                                                   cluster_meta=meta,
+                                                                   regulon_genes=target_genes,
+                                                                   celltype=ct, groupby=groupby)
+                dot_data['regulons'].append(reg)
+                dot_data['cell type'].append(ct)
+                dot_data['percentage'].append(reg_ct_percent)
+                dot_data['avg exp'].append(reg_ct_avg_exp)
+
+        dot_df = pd.DataFrame(dot_data)
+        dot_df.to_csv('dot_df.csv', index=False)
+        g = sns.scatterplot(data=dot_df, size='percentage', hue='avg exp', x='regulons', y='cell type', sizes=(20, 200),
+                            marker='o', palette=palette, legend='full', **kwargs)
+        plt.legend(frameon=False, loc=(1.04, 0))
+        plt.tick_params(axis='both', length=0, labelsize=6)
+        plt.xticks(rotation=90)
+        plt.tight_layout()
+        plt.savefig('dot.png')
+        return g
+
+    @staticmethod
+    def auc_heatmap(auc_mtx: pd.DataFrame, width=8, height=8, fn='auc_heatmap.png', **kwargs):
+        """
+        Plot heatmap for auc value for regulons
+        :param height:
+        :param width:
+        :param auc_mtx:
+        :param fn:
+        :return:
+        """
+        if 'Cell' in auc_mtx.columns:
+            auc_mtx = auc_mtx.set_index('Cell')
+        plt.figsize = (width, height)
+        sns.clustermap(auc_mtx, **kwargs)
+        plt.tight_layout()
+        plt.savefig(fn)
+
+    @staticmethod
+    def plot_2d_reg_stereo(data: StereoExpData, auc_mtx, reg_name: str, **kwargs):
+        """
+        Plot genes of one regulon on a 2D map
+        :param data:
+        :param auc_mtx:
+        :param reg_name:
+        :return:
+        """
+        if '(+)' not in reg_name:
+            reg_name = reg_name + '(+)'
+        cell_coor = data.position
+        auc_zscore = cal_zscore(auc_mtx)
+        # prepare plotting data
+        sub_zscore = auc_zscore[reg_name]
+        # sort data points by zscore (low to high), because first dot will be covered by latter dots
+        zorder = np.argsort(sub_zscore[reg_name].values)
+        # plot cell/bin dot, x y coor
+        sc = plt.scatter(cell_coor[:, 0][zorder], cell_coor[:, 1][zorder], c=sub_zscore[reg_name][zorder], marker='.',
+                         edgecolors='none', cmap='plasma', lw=0, **kwargs)
+        plt.box(False)
+        plt.axis('off')
+        plt.colorbar(sc, shrink=0.35)
+        plt.savefig(f'{reg_name.split("(")[0]}.png')
+        plt.close()
+
+    @staticmethod
+    def plot_2d_reg_h5ad(data: anndata.AnnData, pos_label, auc_mtx, reg_name: str, **kwargs):
+        """
+        Plot genes of one regulon on a 2D map
+        :param pos_label:
+        :param data:
+        :param auc_mtx:
+        :param reg_name:
+        :return:
+
+        Example:
+            plot_2d_reg_h5ad(data, 'spatial', auc_mtx, 'Zfp354c')
+        """
+        if '(+)' not in reg_name:
+            reg_name = reg_name + '(+)'
+        cell_coor = data.obsm[pos_label]
+        auc_zscore = cal_zscore(auc_mtx)
+        # prepare plotting data
+        sub_zscore = auc_zscore[reg_name]
+        # sort data points by zscore (low to high), because first dot will be covered by latter dots
+        zorder = np.argsort(sub_zscore[reg_name].values)
+        # plot cell/bin dot, x y coor
+        sc = plt.scatter(cell_coor[:, 0][zorder], cell_coor[:, 1][zorder], c=sub_zscore[reg_name][zorder], marker='.',
+                         edgecolors='none', cmap='plasma', lw=0, **kwargs)
+        plt.box(False)
+        plt.axis('off')
+        plt.colorbar(sc, shrink=0.35)
+        plt.savefig(f'{reg_name.split("(")[0]}.png')
+        plt.close()
+
+    @staticmethod
+    def rss_heatmap(data: anndata.AnnData,
+                    auc_mtx: pd.DataFrame,
+                    meta: pd.DataFrame,
+                    regulons: list,
+                    save=True,
+                    fn='clusters-heatmap-top5.png',
+                    **kwargs):
+        """
+        Plot heatmap for Regulon specificity scores (RSS) value
+        :param data: 
+        :param auc_mtx: 
+        :param regulons:
+        :param meta:
+        :param save:
+        :param fn:
+        :return: 
+        """
+        meta = pd.read_csv('meta_mousebrain.csv', index_col=0).iloc[:, 0]
+
+        # TODO: adapt to StereoExpData
+        # load the regulon_list from a file using the load_signatures function
+        # regulons = load_signatures(regulons_fn)  # regulons_df -> list of regulon_list
+        # data = add_scenic_metadata(data, auc_mtx, regulons)
+
+        # Regulon specificity scores (RSS) across predicted cell types
+        rss_cellType = regulon_specificity_scores(auc_mtx, meta)
+        rss_cellType.to_csv('regulon_specificity_scores.txt')
+        # Select the top 5 regulon_list from each cell type
+        cats = sorted(list(set(meta)))
+        topreg = []
+        for i, c in enumerate(cats):
+            topreg.extend(
+                list(rss_cellType.T[c].sort_values(ascending=False)[:5].index)
+            )
+        topreg = list(set(topreg))
+
+        # plot z-score
+        auc_zscore = cal_zscore(auc_mtx)
+        sns.set(font_scale=1.2)
+        g = sns.clustermap(auc_zscore[topreg], annot=False, square=False, linecolor='gray', yticklabels=True,
+                           xticklabels=True, vmin=-2, vmax=6, cmap="YlGnBu", figsize=(21, 16), **kwargs)
+        g.cax.set_visible(True)
+        g.ax_heatmap.set_ylabel('')
+        g.ax_heatmap.set_xlabel('')
+        if save:
+            plt.savefig(fn)
+        return g
+
+
+def cal_zscore(auc_mtx: pd.DataFrame) -> pd.DataFrame:
+    """
+    calculate z-score for each gene among cells
+    :param auc_mtx:
+    :return:
+    """
+    func = lambda x: (x - x.mean()) / x.std(ddof=0)
+    auc_zscore = auc_mtx.transform(func, axis=0)
+    auc_zscore.to_csv('auc_zscore.csv', index=False)
+    return auc_zscore
+
+
+def is_regulon_name(reg):
+    """
+    Decide if a string is a regulon_list name
+    :param reg: the name of the regulon
+    :return:
+    """
+    if '(+)' in reg or '(-)' in reg:
+        return True
+
