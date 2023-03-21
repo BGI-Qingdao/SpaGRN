5697ff940dab039a801f31478e759e7531d966c1
YaoLi3 <liyaoo6o@gmail.com>
2023-03-21 15:57:22 +0800

    add argpase

--- spatialGRN/test.py
+++ spatialGRN/test.py
@@ -9,55 +9,73 @@
 @last modified by: Yao LI
 """
 
+import os
 import sys
 import json
+import argparse
 import pandas as pd
 from multiprocessing import cpu_count
 from plot import PlotRegulatoryNetwork
-from regulatory_network import InferenceRegulatoryNetwork
-from plot import PlotRegulatoryNetwork
+from regulatory_network import InferenceRegulatoryNetwork as irn
+from plot import PlotRegulatoryNetwork as prn
 
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='spaGRN tester')
+    parser.add_argument("--data", '-i', type=str, help='experiment data file, in h5ad/loom format')
+    parser.add_argument("--tf", '-t', type=str, help='TF list file')
+    parser.add_argument("--database", '-d', type=str, help='ranked motifs database file, in feather format')
+    parser.add_argument("--motif_anno", '-m', type=str, help='motifs annotation file, in tbl format')
+    parser.add_argument("--method", type=str, default='grnboost', choices=['grnboost', 'hotspot', 'scoexp'], help='method to calculate TF-gene similarity')
+    parser.add_argument("--output", '-o', type=str, help='output directory')
+    args = parser.parse_args(args=None if sys.argv[1:] else ['--help'])
+
+
+
     # supporting files
-    tfs_fn = '/dellfsqd2/ST_OCEAN/USER/liyao1/stereopy/resource/tfs/mm_mgi_tfs.txt'
+    #tfs_fn = '/dellfsqd2/ST_OCEAN/USER/liyao1/stereopy/resource/tfs/mm_mgi_tfs.txt'
     # tfs_fn = '/dellfsqd2/ST_OCEAN/USER/liyao1/stereopy/resource/tfs/test_mm_mgi_tfs.txt'
-    database_fn = '/dellfsqd2/ST_OCEAN/USER/liyao1/stereopy/database/mm10_10kbp_up_10kbp_down_full_tx_v10_clust.genes_vs_motifs.rankings.feather'
-    motif_anno_fn = '/dellfsqd2/ST_OCEAN/USER/liyao1/stereopy/resource/motifs/motifs-v10nr_clust-nr.mgi-m0.001-o0.0.tbl'
-
+    #database_fn = '/dellfsqd2/ST_OCEAN/USER/liyao1/stereopy/database/mm10_10kbp_up_10kbp_down_full_tx_v10_clust.genes_vs_motifs.rankings.feather'
+    #motif_anno_fn = '/dellfsqd2/ST_OCEAN/USER/liyao1/stereopy/resource/motifs/motifs-v10nr_clust-nr.mgi-m0.001-o0.0.tbl'
     # clustering output for stereopy data]
-    meta_fn = '/dellfsqd2/ST_OCEAN/USER/liyao1/stereopy/resource/StereopyData/meta_mousebrain.csv'
-
+    #meta_fn = '/dellfsqd2/ST_OCEAN/USER/liyao1/stereopy/resource/StereopyData/meta_mousebrain.csv'
     # h5ad data
-    fn = '/dellfsqd2/ST_OCEAN/USER/liyao1/stereopy/resource/StereopyData/MouseBrainCellbin.h5ad'
+    #fn = '/dellfsqd2/ST_OCEAN/USER/liyao1/stereopy/resource/StereopyData/MouseBrainCellbin.h5ad'
     # data = InferenceRegulatoryNetwork.load_anndata_by_cluster(fn, 'psuedo_class', ['HBGLU9', 'TEINH12', 'HBSER4'])
-
     # alternative: small h5ad for testing
     # fn = '/dellfsqd2/ST_OCEAN/USER/liyao1/stereopy/resource/StereopyData/test_grn_MouseBrainCellbin.h5ad'
     # stereopy data
     # fn = '/dellfsqd2/ST_OCEAN/USER/liyao1/stereopy/resource/StereopyData/SS200000135TL_D1.cellbin.gef'
     # fn = '/dellfsqd2/ST_OCEAN/USER/liyao1/stereopy/resource/StereopyData/SS200000135TL_D1.raw.gef'
-    data = InferenceRegulatoryNetwork.read_file(fn)
-    # 2023-03-18
-    tfs = InferenceRegulatoryNetwork.load_tfs(tfs_fn)
-    data_genes = data.var_names
-    common_tfs = set(tfs).intersection(set(data_genes))
-    data = data[:, list(common_tfs)]
+
+    # # 2023-03-18
+    # tfs = InferenceRegulatoryNetwork.load_tfs(tfs_fn)
+    # data_genes = data.var_names
+    # common_tfs = set(tfs).intersection(set(data_genes))
+    # data = data[:, list(common_tfs)]
     # data = read_gef(fn, bin_size=200)
 
+    fn = args.data
+    tfs_fn = args.tf
+    database_fn = args.database
+    motif_anno_fn = args.motif_anno
+    out_dir = args.output
+    method = args.method
+    prefix = os.path.join(out_dir, method)
+
+    # load data
+    data = irn.read_file(fn)
+    grn = irn(data)
+    grn_plot = prn(data)
+
     # create grn
-    grn = InferenceRegulatoryNetwork(data)
-    grn.main(database_fn, motif_anno_fn, tfs_fn, num_workers=cpu_count(), cache=False, save=True, method='grnboost',
-             prefix='grnboost')
+    grn.main(database_fn, motif_anno_fn, tfs_fn, num_workers=cpu_count(), cache=False, save=True, method=method, prefix=prefix)
+    grn_plot.auc_heatmap(grn.auc_mtx, fn=f'{method}_auc_heatmap.png')
     # grn.main(database_fn, motif_anno_fn, tfs_fn, num_workers=cpu_count(), cache=False, save=True, method='hotspot', prefix='hotspot')
+    # grn_plot.auc_heatmap(grn.auc_mtx, fn='hotspot_auc_heatmap.png')
+    # grn.main(database_fn, motif_anno_fn, tfs_fn, num_workers=cpu_count(), cache=False, save=True, method='scoexp', prefix='scoexp')
+    # grn_plot.auc_heatmap(grn.auc_mtx, fn='scoexp_auc_heatmap.png')
 
-    # grn_plot = PlotRegulatoryNetwork(data)
 
-    # meta = pd.read_csv(meta_fn)
-    # rdict = json.load(open('regulons.json'))
-    # rdict = grn.regulon_dict
-
-    # grn_plot.dotplot_stereo(data, meta, rdict, ['Zfp354c'], ['VLMC2','ABC','ACNT1'], 'cell_type')
-    # grn_plot.dotplot_anndata(data, grn.gene_names, cluster_label='psuedo_class')
-    # grn_plot.auc_heatmap(grn.auc_mtx)
-    # grn_plot.plot_2d_reg_h5ad(data, 'spatial', grn.auc_mtx, 'Zfp354c')
+    #grn_plot.dotplot_anndata(data, grn.gene_names, cluster_label='psuedo_class')
+    #grn_plot.plot_2d_reg_h5ad(data, 'spatial', grn.auc_mtx, 'Zfp354c')
