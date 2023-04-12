3fd68cf74375df6d3d969e83b0aaf56739f802d5
Lidong <guolidong@genomics.cn>
2023-04-12 22:53:27 +0800

    truely resolve the conflict now

--- spagrn/__init__.py
+++ spagrn/__init__.py
@@ -1,13 +1,8 @@
 # spatialGRN
 
-<<<<<<< HEAD
 __all__ = ['regulatory_network', 'plot', 'spa_logger']
 
 # deprecated to keep older scripts who import this from breaking
 from spagrn.regulatory_network import InferRegulatoryNetwork
 from spagrn.plot import PlotRegulatoryNetwork
 from spagrn.spa_logger import GetLogger
-=======
-from spagrn.regulatory_network import InferRegulatoryNetwork
-from spagrn.plot import PlotRegulatoryNetwork
->>>>>>> f0366592e5ef935c55ce7dbd23390bffe09e5f2a
