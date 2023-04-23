ffe9d4a0532d62eacb40c95344464dbaad85ef3d
YaoLi3 <liyaoo6o@gmail.com>
2023-04-23 18:59:17 +0800

    when setting install_requires, write list instead of reading requirements.txt; when using README.md in long description, twine upload would report errors: 'The description failed to render in the default format of reStructuredText.'

--- setup.py
+++ setup.py
@@ -2,18 +2,32 @@
 import setuptools
 import glob
 import os
+from pathlib import Path
 
 setuptools.setup(
     name="spagrn",
-    version="1.0.1",
+    version="1.0.3",
     author="Yao LI, Lidong GUO, Mengyang XU",
     author_email="liyao1@genomics.cn, guolidong@genomics.cn, xumengyang@genomics.cn",
     url="https://github.com/BGI-Qingdao/SpaGRN",
-    #packages=setuptools.find_packages(),
-    packages=setuptools.find_packages(where="spagrn"),
-    package_dir={"": "spagrn"},
-    py_modules=[
-        os.path.splitext(os.path.basename(path))[0] for path in glob.glob("spagrn/*.py")
+    #long_description=Path('README.md').read_text('utf-8'),
+    python_requires=">=3.7,<3.11",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        "anndata==0.8.0",
+        "pandas>=1.3.5",
+        "scanpy",
+        "seaborn",
+        "matplotlib",
+        "pyscenic==0.12.1",
+        "hotspotsc==1.1.1",
+        "scipy",
+        #"numpy<1.20.0,>=1.16.6",
+        "numpy",
+        "dask",
+        "arboreto",
+        "ctxcore>=0.2.0",
+        "sklearn"
     ],
     classifiers=[
         "Programming Language :: Python :: 3",
@@ -22,6 +36,6 @@ setuptools.setup(
     ],
     license="GPL-3.0+",
     description="A comprehensive tool to infer TF-centered, spatial gene regulatory networks for the spatially resolved transcriptomic data.",
-    platforms='any'
+    platforms='any',
 )
 
