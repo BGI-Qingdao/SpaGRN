92a89aa03407dfcad572acfa23f45ee6a395e876
YaoLi3 <liyaoo6o@gmail.com>
2023-05-27 20:05:23 +0800

    remove PlotRegulatoryNetwork class

--- spagrn/__init__.py
+++ spagrn/__init__.py
@@ -4,7 +4,7 @@ __all__ = ['regulatory_network', 'plot', 'spa_logger']
 
 # deprecated to keep older scripts who import this from breaking
 from spagrn.regulatory_network import InferRegulatoryNetwork
-from spagrn.plot import PlotRegulatoryNetwork
+# from spagrn.plot import PlotRegulatoryNetwork
 from spagrn.spa_logger import GetLogger
 
 COLORS = [
