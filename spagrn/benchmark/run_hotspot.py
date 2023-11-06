4193e4d51bc5e7871f44765ef9946cfbccfdb15d
YaoLi3 <liyaoo6o@gmail.com>
2023-11-06 17:04:34 +0800

    tidy up

--- spagrn/benchmark/run_hotspot.py
+++ spagrn/benchmark/run_hotspot.py
@@ -6,7 +6,6 @@
 import sys
 
 import hotspot
-import numpy as np
 import scanpy as sc
 import pandas as pd
 import seaborn as sns
@@ -15,13 +14,19 @@ import json
 from typing import Union
 
 
-def get_cluster_label(g, df):
+def get_cluster_label(g: sns.matrix.ClusterGrid, df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Extract dendrogram x and y labels
+    :param g:
+    :param df:
+    :return:
+    """
     import scipy
     den = scipy.cluster.hierarchy.dendrogram(g.dendrogram_col.linkage,
                                              labels=df.index,
                                              color_threshold=0.60)
 
-    def get_cluster_classes(den, label='ivl'):
+    def get_cluster_classes(den: dict, label='ivl'):
         from collections import defaultdict
         cluster_idxs = defaultdict(list)
         for c, pi in zip(den['color_list'], den['icoord']):
@@ -53,66 +58,13 @@ def get_cluster_label(g, df):
     return df
 
 
-# alternative:
-def xx(df):
+def get_module_cols(adata, module_scores: pd.DataFrame) -> list:
     """
-    Use pre-computed hierarchy outputs,
-    instead of calculating it in the sns.clustermap function (only output a fig object, not the proper hierarchy tree).
 
-    The safest route is to first compute the linkages explicitly and pass them to the clustermap function,
-    which has row_linkage and col_linkage parameters just for that.
-
-    :param df:
+    :param adata:
+    :param module_scores:
     :return:
     """
-    used_networks = [1, 5, 6, 7, 8, 11, 12, 13, 16, 17]
-    network_pal = sns.cubehelix_palette(len(used_networks),
-                                        light=.9, dark=.1, reverse=True,
-                                        start=1, rot=-2)
-    network_lut = dict(zip(map(str, used_networks), network_pal))
-
-    networks = df.columns.get_level_values("network")
-    network_colors = pd.Series(networks).map(network_lut)
-
-    from scipy.spatial import distance
-    from scipy.cluster import hierarchy
-
-    correlations = df.corr()
-    correlations_array = np.asarray(df.corr())
-
-    row_linkage = hierarchy.linkage(
-        distance.pdist(correlations_array), method='average')
-
-    col_linkage = hierarchy.linkage(
-        distance.pdist(correlations_array.T), method='average')
-
-    cmap = sns.diverging_palette(h_neg=210, h_pos=350, s=90, l=30, as_cmap=True)
-
-    sns.clustermap(correlations, row_linkage=row_linkage, col_linkage=col_linkage, row_colors=network_colors,
-                   method="average",
-                   col_colors=network_colors, figsize=(13, 13), cmap=cmap)
-
-
-# alternative:
-def xxx():
-    import scipy.cluster.hierarchy as sch
-
-    df = pd.read_csv('expression_data.txt', sep='\t', index_col=0)
-
-    # retrieve clusters using fcluster
-    d = sch.distance.pdist(df)
-    L = sch.linkage(d, method='complete')
-    # 0.2 can be modified to retrieve more stringent or relaxed clusters
-    clusters = sch.fcluster(L, 0.2 * d.max(), 'distance')
-
-    # clusters indicices correspond to incides of original df
-    for i, cluster in enumerate(clusters):
-        print(df.index[i], cluster)
-
-    return clusters
-
-
-def get_module_cols(adata, module_scores):
     module_cols = []
     for c in module_scores.columns:
         key = f"Module {c}"
@@ -121,7 +73,14 @@ def get_module_cols(adata, module_scores):
     return module_cols
 
 
-def has_tf(tfs, l, module_id) -> Union[None, str]:
+def has_tf(tfs: list, l: list, module_id: Union[int, str]) -> Union[None, str]:
+    """
+
+    :param tfs:
+    :param l:
+    :param module_id:
+    :return:
+    """
     com_tf = set(tfs).intersection(set(l))
     if len(com_tf) == 1:
         return list(com_tf)[0]
@@ -132,7 +91,13 @@ def has_tf(tfs, l, module_id) -> Union[None, str]:
         print(f'module {module_id}')
 
 
-def create_regulons(hs, tf_names) -> dict:
+def create_regulons(hs: hotspot.Hotspot, tf_names: list) -> dict:
+    """
+
+    :param hs:
+    :param tf_names:
+    :return:
+    """
     mm = list(set(hs.modules))
     mm.remove(-1)
     regs = {}
@@ -144,7 +109,13 @@ def create_regulons(hs, tf_names) -> dict:
     return regs
 
 
-def lc2adj(local_correlations, common_tf_list):
+def lc2adj(local_correlations: pd.DataFrame, common_tf_list: list) -> pd.DataFrame:
+    """
+
+    :param local_correlations:
+    :param common_tf_list:
+    :return:
+    """
     local_correlations['TF'] = local_correlations.columns
     local_correlations = local_correlations.melt(id_vars=['TF'])
     local_correlations.columns = ['TF', 'target', 'importance']
@@ -175,7 +146,7 @@ def main():
     get_module_cols(adata, module_scores)
 
     # create regulons
-    tf_names = ['Adf1', 'Aef1', 'grh', 'kn', 'tll']
+    tf_names = ['Adf1', 'Aef1', 'grh', 'kn', 'tll', 'disco-r', 'Med', 'Dfd', 'br', 'so']
     regulons = create_regulons(hs, tf_names)
     with open('HOTSPOT_regulons.json', 'w') as f:
         json.dump(regulons, f, indent=4)
