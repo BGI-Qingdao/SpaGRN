8d6ce51b99b190c3572c1b03bd6a15d5a4615700
YaoLi3 <liyaoo6o@gmail.com>
2025-04-14 10:32:26 +0800

    update version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from pathlib import Path
 
 setuptools.setup(
     name="spagrn",
-    version="1.1.4-beta",
+    version="1.1.4",
     author="Yao LI",
     author_email="liyao1@genomics.cn",
     url="https://github.com/BGI-Qingdao/SpaGRN",
